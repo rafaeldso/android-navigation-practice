a07fc6b057798e39f1431d31dca6fab724920d50
Rafael Oliveira <rdso@cesar.org.br>
2023-05-12 17:06:39 -0300

    Initial commit

---
+++ app/src/main/java/com/br/rafael/android_navigation_practice/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.br.rafael.android_navigation_practice.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.br.rafael.android_navigation_practice.data.LoginDataSource
+import com.br.rafael.android_navigation_practice.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
